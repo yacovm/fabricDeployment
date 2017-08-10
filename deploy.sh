5841803fc668e8c57b54ecdb61ccd999d9e9a326
yacovm <yacovm@il.ibm.com>
2017-08-10 10:37:34 +0300

    removed master branch reference

--- deploy.sh
+++ deploy.sh
@@ -143,7 +143,7 @@ echo "Deploying configuration"
 for p in $orderer $peers ; do
         ssh $p "pkill orderer; pkill peer" || echo ""
         ssh $p "rm -rf /var/hyperledger/production/*"
-        ssh $p "cd /opt/gopath/src/github.com/hyperledger/fabric ; git reset HEAD --hard && git pull origin master"
+        ssh $p "cd /opt/gopath/src/github.com/hyperledger/fabric ; git reset HEAD --hard && git pull"
         scp -r $p/sampleconfig/* $p:/opt/gopath/src/github.com/hyperledger/fabric/sampleconfig/
 done
 
